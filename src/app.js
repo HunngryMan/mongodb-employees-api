d17fddbd4f6baefa4b537058748003e1862000fb
Angel Bustillos <dinosaurio_adrian@hotmail.com>
2022-07-26 17:41:15 -0500

    CORS added to avoid angular api connection error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
 
 // SETTINGS
 app.set('port', process.env.PORT || 4000)
 
 // MIDDLEWARE
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
